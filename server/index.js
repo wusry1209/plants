5165bf689fa406bdba36f7f7636e70c88224f465
Jake Webb <jakewebbsite@gmail.com>
2019-03-05 20:58:37 -0600

    add endpoints to node server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
+// import express node framework package from /node_modules
 const express = require('express')
-
+// instantiate express
 const app = express()
+
+// import data from local json file
+const data = require('./data.json')
+
+// this is your homepage in html format
+app.get('/', (req, res) => {
+  return res.status(200).sendFile('../client/index.html', {root: __dirname })
+})
+
+// this endpoint returns your plant data in json format
+app.get('/plants', (req, res) => {
+  console.log('REQUESTED!')
+
+  return res.status(200).json(data)
+})
+
+// this catches GET requests to any route that doesn't exist
+app.get('*', (req, res) => {
+  // this escape character: \ says ignore the next character after
+  return res.status(404).send('this resource doesn\'t exist!')
+})
 
 // let the server choose a port for you via environment variables or default to 3000 if no env variables
 const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  console.log('REQUESTED!')
-})
 
 app.listen(port, function() {
   console.log('Server is listening on port: ' + port)
